fd9395c8ecf0691b951b77e7c4c4dc2610c834a9
Edwin Maikara <160368675+edd167@users.noreply.github.com>
2024-09-06 15:12:08 -0500

    3pm 9/6/24

--- Person.java
+++ Person.java
@@ -1,9 +1,15 @@
 public class Pesron{
-    String firstname,lastname,id;
+    Private String firstname,lastname,id;
+
+     String getFirstname();
+     String getLastname();
+     String getId();
 
 }
 public class Main{
     public static void main(String[],args){
-        
+        Pesron p1= new Pesron()
+       System.out.println(p1.getFirstname("edwin"))
+
     }
 }
