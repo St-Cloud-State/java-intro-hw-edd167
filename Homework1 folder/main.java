b0f23f3f5d7c8636117d6e696573f050d728c4dc
Edwin Maikara <160368675+edd167@users.noreply.github.com>
2024-09-07 01:54:05 +0000

    9/6

--- Homework1 folder/main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String[],args){
-        Pesron p1= new Pesron();
-       System.out.println(p1.getFirstname("edwin"));
-       System.out.println(p1.getLastname("Maikara"));
-       System.out.println(p1.getid("56"));
-    }
-}
--- Person.java
+++ Person.java
@@ -25,3 +25,11 @@ public String toString(){
     return myString;
 }
 }
+public class Main{
+    public static void main(String[],args){
+        Pesron p1= new Pesron();
+       System.out.println(p1.getFirstname("edwin"));
+       System.out.println(p1.getLastname("Maikara"));
+       System.out.println(p1.getid("56"));
+    }
+}
