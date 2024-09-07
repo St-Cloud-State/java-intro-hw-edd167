dfc555354f1b6e3ef64cf0ef51c3ff7e48a6c20b
Edwin Maikara <160368675+edd167@users.noreply.github.com>
2024-09-07 01:25:03 +0000

    8pm 9/6

--- Person.java
+++ Person.java
@@ -1,15 +1,35 @@
 public class Pesron{
-    Private String firstname,lastname,id;
+    Private String firstName;
+    Private String lastName;
+    Private String id;
 
-     String getFirstname();
-     String getLastname();
-     String getId();
-
+// constructor
+public Pesron(String firstName,String lastName,String id){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.id=id;
+}
+// accessor methods
+     String getFirstname(){
+        return firstName;
+     }
+     String getLastname(){
+     return lastName;
+     }
+     String getId(){
+        return id;
+     }
+// toString method 
+public String toString(){
+    String myString = firstName +"\n"+lastName+ "\n" +id"\n"
+    return myString;
+}
 }
 public class Main{
     public static void main(String[],args){
-        Pesron p1= new Pesron()
-       System.out.println(p1.getFirstname("edwin"))
-
+        Pesron p1= new Pesron();
+       System.out.println(p1.getFirstname("edwin"));
+       System.out.println(p1.getLastname("Maikara"));
+       System.out.println(p1.getid("56"));
     }
 }
