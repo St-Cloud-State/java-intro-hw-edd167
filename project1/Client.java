961bb4c93226678be4aa0c38416765add655d098
Edwin Maikara <160368675+edd167@users.noreply.github.com>
2024-09-09 00:25:44 +0000

    7.25 9/8

--- project1/Client.java
+++ project1/Client.java
@@ -16,4 +16,46 @@ private ArrayList<Order>orders;
  this.transactions=new ArrayList<>();
  this.wishlist=new ArrayList<>();
  }
+ //getters and setter methods
+ //method to add transactions 
+ public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+      //method to add product to wishlist
+    public void addToWishlist(Product product, int quantity) {
+        wishlist.add(new WishlistItem(product, quantity));
+    }
+    // method to get products in wishlist
+    public ArrayList<WishlistItem> getWishlist() {
+        return wishlist;
+    }
+  //method to add orders
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+// method returns clientId
+    public int getClientId() {
+        return clientId;
+    }
+//method to get name of client
+    public String getName() {
+        return name;
+    }
+//method to debit customer balance
+    public void debitBalance(double amount) {
+        balance -= amount;
+    }
+//method to credit customer balance 
+    public void creditBalance(double amount) {
+        balance += amount;
+    }
+//method to get customer balance
+    public double getBalance() {
+        return balance;
+    }
+//method to get all transactions in the system
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
 }
