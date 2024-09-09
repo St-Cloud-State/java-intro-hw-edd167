e97786f79f928ba8a7212d641b888debbf25cbdb
Edwin Maikara <160368675+edd167@users.noreply.github.com>
2024-09-09 01:19:08 +0000

    9/8

---
+++ project1/Order.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+public class Order{
+    private static int idCounter=0;
+    private int orderId;
+    private Client client;
+    private HashMap<product,Interger>orderItems;
+    private double totalCost;
+
+    public Order(Client client){
+        this.orderId=++idCounter;
+        this.client=client;
+        this.totalCost=0;
+        this.orderItems=new HashMap<>();
+
+    }
+    public void addProduct(Product product,int quantity){
+    orderItems.put(product,quantity);
+    totalCost=+ product.getSalePrice()*quantity;
+    }
+    public double getTotalCost(){
+        return totalCost;
+    }
+}
