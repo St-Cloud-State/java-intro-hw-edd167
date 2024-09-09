45684897f65639fef171775616e27317b3ad99b9
Edwin Maikara <160368675+edd167@users.noreply.github.com>
2024-09-09 00:15:25 +0000

    9/8 ss

--- project1/Client.java
+++ project1/Client.java
@@ -1,1 +1,19 @@
-Public class
+Public class Client{
+private static int idCounter=0;
+private int clientId;
+private String name;
+private String address;
+private double balance;
+private ArrayList<Transactions>transactions;
+private ArrayList<WishListItems>wishlist;
+private ArrayList<Order>orders;
+
+ public Client(String name,String address){
+ this.clientId=++idCounter;
+ this.name=name;
+ this.address=address;
+ this.balance=0;
+ this.transactions=new ArrayList<>();
+ this.wishlist=new ArrayList<>();
+ }
+}
